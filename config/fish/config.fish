306da7e82cca18dc7de5ba3a04ccab0daff42576
Sam McDavid <sam@mcdavid.us>
2025-06-20 18:58:22 -0400

    ASDF updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# asdf setup
+set -x ASDF_DATA_DIR /Users/sam/.asdf
+fish_add_path $ASDF_DATA_DIR/shims
+source /usr/local/Cellar/asdf/0.18.0/libexec/asdf.fish
