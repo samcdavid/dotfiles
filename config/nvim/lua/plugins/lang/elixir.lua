098a2193e8496ec7a8da7faf0fa6f11f65e793b2
Sam McDavid <sam@mcdavid.us>
2025-06-20 19:43:29 -0400

    Use lexical

--- config/nvim/lua/plugins/lang/elixir.lua
+++ config/nvim/lua/plugins/lang/elixir.lua
@@ -1,55 +1,14 @@
 return {
   {
-    "elixir-tools/elixir-tools.nvim",
-    version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local elixir = require("elixir")
-      local elixirls = require("elixir.elixirls")
-
-      elixir.setup({
-        nextls = {
-          enable = false,
-          port = 9000,
-          init_options = {
-            mix_env = "dev",
-            mix_target = "host",
-            experimental = {
-              completions = {
-                enable = true,
-              },
-            },
-          },
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lexical = {
+          cmd = { vim.fn.expand("~/.local/share/nvim/lexical/_build/dev/package/lexical/bin/start_lexical.sh") },
+          settings = {},
         },
-        credo = { enable = true },
-        elixirls = {
-          enable = true,
-          settings = elixirls.settings({
-            autoBuild = true,
-            signatureAfterComplete = true,
-            dialyzerEnabled = false,
-            enableTestLenses = true,
-          }),
-          on_attach = function(client, bufnr)
-            vim.keymap.set("n", "<_>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("n", "<_>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("v", "<_>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-          end,
-        },
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+      },
     },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
-        "elixir-ls",
-      })
-    end,
   },
   {
     "nvim-neotest/neotest",
